68db7de90faf196a905e40be1e7896cda6b57d3a
alfredocassanj-hub <alfredocassanj@gmail.com>
2026-07-11 17:30:10 +0100

    s

---
+++ scripts/temples.js
@@ -0,0 +1,19 @@
+const year = document.querySelector("#year");
+
+year.textContent = new Date().getFullYear();
+
+document.querySelector("#lastModified").textContent =
+`Last Modification: ${document.lastModified}`;
+
+const menuButton = document.querySelector("#menu");
+
+const navigation = document.querySelector(".navigation");
+
+menuButton.addEventListener("click", () => {
+
+navigation.classList.toggle("open");
+
+menuButton.textContent =
+navigation.classList.contains("open") ? "✖" : "☰";
+
+});
