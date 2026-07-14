d5452e406fef15fee5ee2f570e7ea8d80b048916
alfredocassanj-hub <alfredocassanj@gmail.com>
2026-07-14 05:51:51 +0100

    d

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,30 @@
+const menuButton = document.querySelector("#menu");
+const navigation = document.querySelector("#navigation");
+
+
+if (menuButton && navigation) {
+
+    menuButton.addEventListener("click", () => {
+
+        navigation.classList.toggle("open");
+
+
+        if (menuButton.textContent.trim() === "☰") {
+
+            menuButton.textContent = "✖";
+
+        } else {
+
+            menuButton.textContent = "☰";
+
+        }
+
+    });
+
+}
+
+
+
 const year = document.querySelector("#currentyear");
 
 if (year) {
@@ -5,19 +32,12 @@ if (year) {
 }
 
 
+
 const lastModified = document.querySelector("#lastModified");
 
 if (lastModified) {
-    lastModified.textContent = `Last Modification: ${document.lastModified}`;
+
+    lastModified.textContent =
+        "Last Modification: " + document.lastModified;
+
 }
-
-
-const menuButton = document.querySelector("#menu");
-const navigation = document.querySelector(".navigation");
-
-
-menuButton.addEventListener("click", () => {
-
-    navigation.classList.toggle("open");
-
-});
