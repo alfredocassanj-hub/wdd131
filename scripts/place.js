e22eb544eb51b60d0d641e4b5aa611390942e850
alfredocassanj-hub <alfredocassanj@gmail.com>
2026-07-21 14:50:06 +0100

    Au

--- scripts/place.js
+++ scripts/place.js
@@ -9,32 +9,22 @@ document.querySelector("#lastModified").textContent =
     document.lastModified;
 
 
-
 // Weather static values
 
 const temperature = 8; // Celsius
 const windSpeed = 15;  // km/h
 
 
-
 // Wind Chill calculation function
 
 function calculateWindChill(temp, speed){
-
-    return (
-        13.12 +
-        0.6215 * temp -
-        11.37 * Math.pow(speed, 0.16) +
-        0.3965 * temp * Math.pow(speed, 0.16)
-    ).toFixed(1);
-
+    return (13.12 + 0.6215 * temp - 11.37 * Math.pow(speed, 0.16) + 0.3965 * temp * Math.pow(speed, 0.16)).toFixed(1);
 }
 
 
 // Check conditions before calling function
 
 let windChill = "N/A";
-
 
 if(temperature <= 10 && windSpeed > 4.8){
 
