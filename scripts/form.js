01ccfc7752a021e8ddd834f521d5dee743105361
alfredocassanj-hub <alfredocassanj@gmail.com>
2026-08-04 17:15:20 +0100

    F

---
+++ scripts/form.js
@@ -0,0 +1,49 @@
+const products = [
+
+{
+id: "p001",
+name: "Smart Watch"
+},
+
+{
+id: "p002",
+name: "Laptop Computer"
+},
+
+{
+id: "p003",
+name: "Wireless Headphones"
+},
+
+{
+id: "p004",
+name: "Smart Phone"
+},
+
+{
+id: "p005",
+name: "Tablet"
+}
+
+];
+
+const productSelect = document.querySelector("#product");
+
+
+
+products.forEach(product => {
+
+
+let option = document.createElement("option");
+
+
+option.value = product.id;
+
+
+option.textContent = product.name;
+
+
+productSelect.appendChild(option);
+
+
+});
